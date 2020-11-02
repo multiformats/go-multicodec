e3345f4a7f10c10ebf25dc5950f973e4b83a7814
Daniel Martí <mvdan@mvdan.cc>
2020-11-02 17:15:49 +0000

    minor adjustments after Dennis's work
    First, use the standard PL open source licensing, which Dennis agreed to
    when he shared his code.
    
    Second, update the README. For example, some bits still referred to the
    now removed GitHub Action, and it is not necessary to spell out what 'go
    generate' does in two places.
    
    Third, move gen/gen.go to just gen.go, as a directory is unnecessary.
    
    Finally, rename init.go to doc.go and add a short package godoc.

--- doc.go
+++ doc.go
@@ -1,5 +1,6 @@
+// Package multicodec exposes the table of multicodecs as Go constants.
 package multicodec
 
-//go:generate go run ./gen/gen.go
+//go:generate go run gen.go
 //go:generate gofmt -w codec.go
 //go:generate stringer -type=Codec -linecomment
